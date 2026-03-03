ac9c8389af057771cca0e60ce783b2b16fe0d007
ExtraTNT <sven.hugi@swisstxt.ch>
2026-03-03 09:55:49 +0100

    well

--- 02/my_args.c
+++ 02/my_args.c
@@ -5,6 +5,4 @@ int main(int argc, char* argv[]) {
         printf("%d: %s\n", i, argv[i]);
     }
     return 0;
-
-    return 0;
 }
---
+++ 02/string.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("\n\\0 as int: %d\n", '\0');
+
+    return 0;
+}
