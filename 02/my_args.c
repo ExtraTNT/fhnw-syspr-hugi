ed20d0abfb5a37a799e64ecf609e6f2c080644d2
ExtraTNT <sven.hugi@swisstxt.ch>
2026-03-03 10:15:53 +0100

    funny and hacky

--- 02/my_args.c
+++ 02/my_args.c
@@ -4,5 +4,10 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < argc; i++) {
         printf("%d: %s\n", i, argv[i]);
     }
+
+    for (char** p = argv; *p != NULL; p++){
+        printf("%lx: %s\n", p - argv, *p);
+    }
+
     return 0;
 }
